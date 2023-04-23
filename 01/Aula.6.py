40dcc6e3aa76f30311eceeac85f31704797c9a9d
Yasmim Moraes <96023730+yasmimmoraes@users.noreply.github.com>
2023-04-23 19:57:05 -0300

    Update Aula.6.py

--- 01/Aula.6.py
+++ 01/Aula.6.py
@@ -1,4 +1,4 @@
 num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
+num2 = int(input("digite o segundo número: "))
 soma = (num1 + num2)
 print("A soma entre {0} e {1} vale {2}".format(num1, num2, soma))
