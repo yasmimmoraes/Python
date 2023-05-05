5a851f1b4d09cd5eefddec3f0c47add09dd61870
YasmimMoraes <moraesyasmim092@gmail.com>
2023-05-05 15:35:30 -0300

    ooooohhh

---
+++ 01/Aula.07.exerc.py
@@ -0,0 +1,4 @@
+# Programa que leia um númmero inteiro e mostre
+# o seu antecessor e sucessor 
+n = int(input("Digite um número: "))
+print(f"O antecessor de {n} é {n-1} e o seu sucessor é {n+1}")
---
+++ 01/aula.7.exer2.py
@@ -0,0 +1,4 @@
+# Algoritmo que leia um número e mostre
+# seu dobro, triplo e raiz quadrada
+n = int(input("Digite um número: "))
+print(f"O dobro de {n} é {n*2}, o triplo é {n*3} \n e a raiz quadrada é {n**(1/2)}")
