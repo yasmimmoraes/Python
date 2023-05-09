208f18fce01a65406dd8832e870975a858a2b61c
YasmimMoraes <moraesyasmim092@gmail.com>
2023-05-09 09:47:49 -0300

    1

--- 01/aula12.py
+++ 01/aula12.py
@@ -1,3 +1,11 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na 
 # tela o nome do escolhido.
+from random import choice
+al = input("Primeiro aluno: ")
+al2 = input("Segundo aluno: ")
+al3 = input("Terceiro aluno: ")
+al4 = input("Quarto aluno: ")
+lista = [al, al2, al3, al4]
+escolhido = choice(lista)
+print(f"O aluno escolhido foi {escolhido}")
